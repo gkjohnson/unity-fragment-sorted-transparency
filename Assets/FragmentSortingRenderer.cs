7017bdbf1c48e5783d70fb7a2581e248f724af1c
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-11-11 12:55:47 -0800

    Add the Unity project

---
+++ Assets/FragmentSortingRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentSortingRenderer : MonoBehaviour {
+
+    Material material = null;
+
+    void OnEnable() { }
+    void OnRenderObject() { }    
+}
